5a2efccf78dab4b52c339eeee625822c10016839
mattkrozel <mattkrozel@gmail.com>
2023-08-21 12:29:31 -0700

    submit final

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
 binary_tree_t *sibling(binary_tree_t *node);
+
 /**
  * *binary_tree_uncle - finds uncle of node
  * @node: node to find uncle for
@@ -13,11 +14,11 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	return (sibling(node->parent));
 }
 /**
- * *binary_tree_sibling - fins siblinng of node
+ * *sibling - fins siblinng of node
  * @node: pointer to node to find sibling
  * Return: pointer to new sibling or NULL
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
