d945e44773f3a5d96f562c51c6a7bb0a162e35e4
mattkrozel <mattkrozel@gmail.com>
2023-08-21 10:39:23 -0700

    task 7 submit

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - prints binary tree values in preorder
+ * @tree: node to be printed
+ * @func: prints the number
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_preorder(tree->left, func);
+	func(tree->n);
+	binary_tree_preorder(tree->right, func);
+}
