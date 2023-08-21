16efb656ce1356d65ea6786be7888ce7506e4af0
mattkrozel <mattkrozel@gmail.com>
2023-08-21 11:12:30 -0700

    submit task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,11 +6,11 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0;
+	size_t size = 1;
 
 	if (!tree)
 		return (0);
-	size += 1 + binary_tree_size(tree->left);
-	size += 1 + binary_tree_size(tree->right);
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
 	return (size);
 }
