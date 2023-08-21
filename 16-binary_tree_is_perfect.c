aca885165b2fd6eb076abebf27d5fa6e1938a579
mattkrozel <mattkrozel@gmail.com>
2023-08-21 12:00:41 -0700

    submit task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_perfect - checks if binary tree is perfect
+ * @tree: pointer to root node of tree
+ * Return: 0 if null
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (((binary_tree_is_full(tree) == 1) &&
+				(binary_tree_balance(tree) == 0))
+		return (1);
+	return (0);
+}
+/**
  * binary_tree_is_full - checks if binary tree is full
  * @tree: pointer to root node of tree
  * Return: 1 if full else 0
@@ -42,12 +56,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (tree->left)
-		left += 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right += 1 + binary_tree_height(tree->right);
+	left += 1 + binary_tree_height(tree->left);
+	right += 1 + binary_tree_height(tree->right);
 	if (left > right)
 		return (left);
-	else
-		return (right);
+	return (right);
 }
