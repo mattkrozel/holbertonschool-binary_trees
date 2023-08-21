1cecbc906bdc630e8722e1c76ea51e2a149b5925
mattkrozel <mattkrozel@gmail.com>
2023-08-21 09:53:39 -0700

    task 0 submit

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = value;
-	new_node->parent;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
 	return (new_node);
