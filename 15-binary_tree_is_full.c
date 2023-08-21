f9296f640e377789eecf2e42ae86f26bdcc7ff5a
mattkrozel <mattkrozel@gmail.com>
2023-08-21 11:38:14 -0700

    submit task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if binary tree is full
+ * @tree: pointer to root node of tree
+ * Return: 1 if full else 0
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left)	&&
+				binary_tree_is_full(tree->right));
+	return (0);
+}
