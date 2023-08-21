bb5b0d0cf1aef0fddd365bde9dd0df461569f90e
mattkrozel <mattkrozel@gmail.com>
2023-08-21 12:37:53 -0700

    submit final

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -24,7 +24,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (!tree->left && !tree->right)
-		return (1);
+		return (0);
 	if (tree->left && tree->right)
 		return (binary_tree_is_full(tree->left)	&&
 				binary_tree_is_full(tree->right));
